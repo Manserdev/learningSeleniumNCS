f17b22797607d54d8d8d936a6011f0f30ae0b564
Tim Manser <manserdev@protonmail.com>
2023-06-28 13:32:20 +1000

    first commit learning selenium with NCS

---
+++ src/test/java/au/com/ncs/seleniumTests.java
@@ -0,0 +1,108 @@
+package au.com.ncs;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class seleniumTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+
+        setWebDriver();
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/#/");
+        driver.manage().window().maximize();
+
+    }
+
+
+    @Test
+    public void enterKeysIntoForenameTest() {
+
+        driver.findElement(By.id("forename")).sendKeys("1234");
+    }
+
+    @Test
+    public void clickSubmitForenameTest() {
+
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Test
+    public void checkWebPlaygroundHeaderTest() {
+
+        WebElement header = driver.findElement(By.tagName("h1"));
+        assertEquals("Web Playground", header.getText());
+    }
+
+    @Test
+    public void clickMeDownTest() {
+        //Arrange
+
+        //Act
+        WebElement buttonElement = driver.findElement(By.cssSelector("div > a.anibtn"));
+        buttonElement.click();
+
+        //Assert
+        new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.textToBe(By.cssSelector("div > a.transition-y"), "CLICK ME UP!"));
+
+    }
+
+    @Test
+    public void ClickMeUpTest() {
+
+        //Arrange
+        WebElement buttonElement = driver.findElement(By.cssSelector("div > a.anibtn"));
+        buttonElement.click();
+        new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.textToBe(By.cssSelector("div > a.transition-y"), "CLICK ME UP!"));
+
+        //Act
+        buttonElement.click();
+
+        //Assert
+        new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.textToBe(By.cssSelector("div > a.anibtn"), "CLICK ME DOWN!"));
+    }
+
+    @Test
+    public void FillInFormTest() {
+
+        //Arrange
+        driver.findElement(By.cssSelector("[aria-label=forms]")).click();
+        assertEquals("Forms", driver.findElement(By.cssSelector("h1.mb-3")).getText());
+
+
+        //Act
+        driver.findElement(By.tagName("#name")).sendKeys("Timothy Manser");
+
+        //Assert
+
+    }
+
+    @AfterEach
+    public void clean() {
+
+        driver.quit();
+
+    }
+
+    public void setWebDriver() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+}
