8cbb57e85f5c6f757a86827d7fc464e433115449
Tim Manser <manserdev@protonmail.com>
2023-06-28 17:02:08 +1000

    lalala last commit

--- src/test/java/au/com/ncs/model/Form.java
+++ src/test/java/au/com/ncs/model/Form.java
@@ -3,7 +3,13 @@ package au.com.ncs.model;
 import org.openqa.selenium.WebDriver;
 
 public class Form {
+
+    private  WebDriver driver;
+
     public Form(WebDriver driver) {
+
+
+        this.driver = driver;
     }
 
     public void enterName(String name) {
